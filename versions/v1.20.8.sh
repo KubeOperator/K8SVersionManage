0ee9a6ca4c5bf9b7b0aa345296e2344ebe25b634
wanghe-fit2cloud <wanghe@fit2cloud.com>
2021-11-18 15:34:20 +0800

    feat: 调整组件版本

--- versions/v1.20.8.sh
+++ versions/v1.20.8.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-docker_version="19.03.15"
+docker_version="20.10.7"
 etcd_version="v3.4.14"
 containerd_version="1.4.3"
-calico_version="v3.16.5"
+calico_version="v3.18.4"
 flannel_version="v0.13.0"
 coredns_version="1.7.0"
 traefik_ingress_version="v2.4.8"
 nginx_ingress_version="0.33.0"
-metrics_server_version="v0.3.6"
+metrics_server_version="v0.5.0"
 helm_v2_version="v2.17.0"
 helm_v3_version="v3.6.0"
 cni_version='v0.8.6'
